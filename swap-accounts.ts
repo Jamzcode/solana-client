1a14ffd462dad3244520acec4e663620eb81ad8f
Jamzcode <129902520+Jamzcode@users.noreply.github.com>
2024-05-10 18:24:25 -0500

    created keypair for Token Swap State Account

---
+++ swap-accounts.ts
@@ -0,0 +1,46 @@
+//Generate Account A liquidity pool
+import { Transaction, Keypair, SystemProgram } from "@solana/web3.js";
+import { TokenSwap, TOKEN_SWAP_PROGRAM_ID, TokenSwapLayout } from "@solana/spl-token-swap";
+
+// const transaction = new Transaction()
+const tokenSwapStateAccount = Keypair.generate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Swap pool authority
+//Token account A
+//Token account B
+//Pool token mint
+//Pool token account
+//Pool token fee account
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Generate Account B liquidity pool
